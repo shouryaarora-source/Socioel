c23e1e8a205aef563dd48db1d22cf938f4626ba6
SHOURYA <SHOURYAARORA9@GMAIL.COM>
2026-06-28 20:27:07 +0530

    render

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -122,6 +122,7 @@ app.use((req, res, next) => {
         origin.startsWith("http://127.0.0.1:") ||
         origin.startsWith("http://192.168.") ||
         origin.startsWith("https://socioel.onrender.com") ||
+        origin.startsWith("https://www.socioel.com") ||
         origin.startsWith("http://10."))
     ) {
       next();
